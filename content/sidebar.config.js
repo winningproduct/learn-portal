a3666c98cdc680f0858a8533501d8c5c00e2f870
Ashan Fernando <Ashan Fernando>
2020-05-20 19:59:10 +0530

    Adding sidebar.config

---
+++ content/sidebar.config.js
@@ -0,0 +1,20 @@
+const config = {
+      forcedNavOrder: [
+        '/1-introduction',
+        '/2-explore',
+        '/3-focus',
+        '/4-immerse',
+        '/5-plan',
+        '/6-build',
+        '/7-stabilize',
+        '/8-optimize',
+        '/9-harvest',
+        '/10-retire',
+        '/FormattingGuide'
+      ],
+      collapsedNav: [
+        '/FormattingGuide'
+      ]
+  };
+  
+  module.exports = config;
